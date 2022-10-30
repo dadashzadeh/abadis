d549009d89a92be738b515a1bccdab68fd8abb7a
dadashzadeh <38451248+dadashzadeh@users.noreply.github.com>
2022-10-31 02:22:42 +0330

    Update abadis.py

--- abadis.py
+++ abadis.py
@@ -10,90 +10,78 @@ headers = {
 r = requests.Session()
 
 
-class abadis:
-    '''دریافت متن از سایت آبادیس'''
+class Abadis:
+    '''Receive text from Abadis site'''
 
-    def __init__(self, text=None, listtext=None):
+    def __init__(self, text=None, list_text=None):
         self.text = text
-        self.listtext = listtext
+        self.list_text = list_text
 
     def get_data_html(self, text: str) -> str:
-        ''' دریافت html '''
         urlabadis = f"https://abadis.ir/fatofa/{text}"
         page = r.get((urlabadis), headers=headers)
         soup = BeautifulSoup(page.content, 'lxml')
         return soup
 
-    def equivalent_text(self, text: str) -> str:
-        '''
-        دریافت کلمات برابر پارسی
-
-        '''
-        soup = abadis().get_data_html(text)
-        if soup.find("div", attrs={"class": "boxMain"}):
-            result_nameleg = soup.find("div", attrs={"class": "boxMain"})
+    def get_equivalent_persian_text(self, text: str) -> str:
+        soup = Abadis().get_data_html(text)
+        
+        if soup.find("div", {"class": "boxMain"}):
+            html_content = soup.find("div", {"class": "boxMain"})
             try:
-                barbar = result_nameleg.find(
+                equivalent = html_content.find(
                     "b", string={"برابر پارسی"}).next_sibling.strip(": ")
-                barbar = barbar.replace("، ", "|")
-                barbar = barbar.replace(" ", "")
+                equivalent_replace = equivalent.replace("، ", "|")
+                equivalent_text = equivalent_replace.replace(" ", "")
             except:
-                barbar = None
-        else:
-            barbar = None
-
-        return barbar
-
-    def general_encyclopedia(self, text: str) -> str:
-        '''
-        دریافت دانشنامه عمومی
-
-        '''
-        p = []
-        soup = abadis().get_data_html(text)
-        if soup.find("div", attrs={"t": "دانشنامه عمومی"}):
-            s = soup.find("div", attrs={"t": "دانشنامه عمومی"})
-            for ddd in s.find_all("div", attrs={"class": "wikilink"}):
-                ddd = ddd.find("a").text
-                p.append(ddd)
+                return None
         else:
             return None
-        return p
 
-    def comment(self, text: str) -> str:
-        '''
-        مشاهده کامنت که بیشترین لایک داره
+        return equivalent_text
 
-        '''
-        soup = abadis().get_data_html(text)
-        s = soup.find("div", {"id": "cmts"})
-        text_hrml = str(s)
+    def get_general_encyclopedia(self, text: str) -> str:
+        list_encyclopedia = []
+        
+        soup = Abadis().get_data_html(text)
+        if soup.find("div", {"t": "دانشنامه عمومی"}):
+            html_content = soup.find("div", {"t": "دانشنامه عمومی"})
+            for list_text in html_content.find_all("div", {"class": "wikilink"}):
+                text_encyclopedia = list_text.find("a").text
+                list_encyclopedia.append(text_encyclopedia)
+        else:
+            return None
 
-        number_list = re.findall(r"[l]..\d+", text_hrml)
+        return list_encyclopedia
+
+    def get_the_most_liked_comment(self, text: str) -> str:
+        soup = Abadis().get_data_html(text)
+        html_content = soup.find("div", {"id": "cmts"})
+
+        # Find the number of likes
+        number_list = re.findall(r"[l]..\d+", str(html_content))
         replace = [list.replace('l=\"', '').strip() for list in number_list]
+        
+        # The most number of likes
         maxnumber = max(replace, key=lambda value: int(value))
-        return s.find("div", {"l": maxnumber}).text
+        return html_content.find("div", {"l": maxnumber}).text
 
-    def equivalent_list(self, listtext: list) -> list:
-        '''
-        ورودی لیست و خروجی لیست 
-        دریافت کلمات برابر پارسی
+    def get_equivalent_persian_list(self, list_text: list) -> list:
+        equivalent_persian_list = []
 
-        '''
-        listbarbar = []
-        for listtexts in listtext:
-            soup = abadis().get_data_html(listtexts)
-            if soup.find("div", attrs={"class": "boxMain"}):
-                result_nameleg = soup.find("div", attrs={"class": "boxMain"})
+        for list_texts in list_text:
+            soup = Abadis().get_data_html(list_texts)
+            if soup.find("div", {"class": "boxMain"}):
+                html_content = soup.find("div", {"class": "boxMain"})
                 try:
-                    barbar = result_nameleg.find(
+                    equivalent_list = html_content.find(
                         "b", string={"برابر پارسی"}).next_sibling.strip(": ")
-                    barbar = barbar.replace("، ", "|")
-                    barbar = barbar.replace(" ", "")
-                    listbarbar.append(barbar)
+                    equivalent_replace = equivalent_list.replace("، ", "|")
+                    equivalent_replace = equivalent_replace.replace(" ", "")
+                    equivalent_persian_list.append(equivalent_replace)
                 except:
-                    listbarbar.append("None")
+                    equivalent_persian_list.append("None")
             else:
-                listbarbar.append("None")
+                equivalent_persian_list.append("None")
 
-        return listbarbar
+        return equivalent_persian_list
